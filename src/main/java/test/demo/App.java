596f9c423b186cd3e68e28c0ee5db6c421f5ca39
samuelpolagani <Samuel.polagani@gmail.com>
2022-07-24 11:25:49 +0530

    modified App.java

--- src/main/java/test/demo/App.java
+++ src/main/java/test/demo/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "\nHello World!, This is Demo Project \n" );
+        System.out.println( "\n Hello World!, This is Demo Project  \n" );
     }
 }
